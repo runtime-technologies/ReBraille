6ee91ab6e6c0c328260dcd883c1e17ef203e15f2
Srijith <srijith96@gmail.com>
2022-03-06 01:25:55 -0500

    Add more braille maps to dictionary; Change from active-high to active-low; Verify code on sample string: WORKS OKgit add braille_disp.py test_ping.py

--- src/braille_disp.py
+++ src/braille_disp.py
@@ -10,57 +10,40 @@ import Jetson.GPIO as GPIO
 # setup GPIO pins on Jetson
 GPIO.setmode(GPIO.BOARD)
 #channels = [21, 22, 23, 24, 26, 29, 31, 32]
-channels = [31, 32, 33, 35, 36, 37, 38, 40]
-GPIO.setup(channels, GPIO.OUT)
-GPIO.output(channels, GPIO.LOW)
+#channels = [31, 32, 33, 35, 36, 37, 38, 40]
+channels = [11, 15, 19, 23, 29, 31, 35, 40]
+GPIO.setup(channels, GPIO.OUT, initial=GPIO.LOW)
+#GPIO.output(channels, GPIO.LOW)
 
 # ------------------------------------------------------------------------------
-SLEEP_TIME_SEC = 2
+SLEEP_TIME_SEC = 1
 # ------------------------------------------------------------------------------
 braille_map = dict()
 
 # letters and corresponding braille sequences
-letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ,.;!"
-brailles = [ 0b100000,
-             0b101000,
-             0b110000,
-             0b110100,
-             0b100100,
-             0b111000,
-             0b111100,
-             0b101100,
-             0b011000,
-             0b011100,
-             0b100010,
-             0b101010,
-             0b110010,
-             0b110110,
-             0b100110,
-             0b111010,
-             0b111110,
-             0b101110,
-             0b011010,
-             0b011110,
-             0b100011,
-             0b101011,
-             0b011101,
-             0b110011,
-             0b110111,
-             0b100111,
-             0b000000,
-             0b001000,
-             0b001101,
-             0b001010,
-             0b001110]
+letters = r"""abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ,.;:!()+&_"'?"""
+small_brailles = [ 0b10000000, 0b10100000, 0b11000000, 0b11010000, 0b10010000,
+                   0b11100000, 0b11110000, 0b10110000, 0b01100000, 0b01110000,
+                   0b10001000, 0b10101000, 0b11001000, 0b11011000, 0b10011000,
+                   0b11101000, 0b11111000, 0b10111000, 0b01101000, 0b01111000,
+                   0b10001100, 0b10101100, 0b01110100, 0b11001100, 0b11011100,
+                   0b10011100 ]
+cap_brailles = [(i + 2) for i in small_brailles]
+num_brailles = [0b01001100, 0b10000100, 0b10100100, 0b11000100,
+                0b11010100, 0b10010100, 0b11100100, 0b11110100,
+                0b10110100, 0b01100100 ]
+punct_brailles = [ 0b00000000, 0b00100000, 0b00110100, 0b00101000, 0b00110000,
+                   0b00111000, 0b00111100, 0b01010010, 0b00110010, 0b11101100,
+                   0b00100001, 0b00011100, 0b00001000, 0b00101100 ]
+brailles = [*small_brailles, *cap_brailles, *num_brailles, *punct_brailles]
 
-for l, b in zip(letters, brailles):
-    braille_map[l] = b
+braille_map = {l: b for (l, b) in zip(letters, brailles)}
 
 print(braille_map)
 # ------------------------------------------------------------------------------
 # test out for a sample string
-sample = "Hello world"
-sample = sample.upper()
+sample = "Hello world! This is RunTime, over."
+#sample = sample.upper()
 
 sample_braille = [0 for c in sample]
 for i, c in enumerate(sample):
@@ -72,8 +55,8 @@ print(braille_bin_list)
 # ------------------------------------------------------------------------------
 def binlist_to_gpio(binlist):
     def __bin_to_gpioHL(bin_str):
-       #return [GPIO.HIGH if ch is '1' else GPIO.LOW for ch in bin_str ] 
-       return ['high' if ch == '1' else 'low' for ch in bin_str ] 
+       return [GPIO.LOW if ch is '1' else GPIO.HIGH for ch in bin_str ] 
+       #return ['high' if ch == '1' else 'low' for ch in bin_str ] 
     gpio_list = [__bin_to_gpioHL(bin_str) for bin_str in binlist]
     return gpio_list
 print(binlist_to_gpio(braille_bin_list))
@@ -83,7 +66,7 @@ gpio_sequence = binlist_to_gpio(braille_bin_list)
 
 for gpio_out in gpio_sequence:
     GPIO.output(channels, gpio_out)
-    print(channels, gpio_out)
+    print(gpio_out)
     sys.stdout.flush()
     time.sleep(SLEEP_TIME_SEC)
 # ------------------------------------------------------------------------------
--- src/test_ping.py
+++ src/test_ping.py
@@ -3,13 +3,13 @@ import time
 
 SLEEP_TIME = 2
 GPIO.setmode(GPIO.BOARD)
-channels = [5, 11, 15, 19]#, 32, 33, 35, 36, 37, 38, 40]
+channels = [11, 15, 23, 31, 29, 19]#, 32, 33, 35, 36, 37, 38, 40]
 
-gpio_hl_list = [GPIO.LOW, GPIO.HIGH, GPIO.HIGH, GPIO.LOW]
-gpio_hl_list2 = [GPIO.HIGH, GPIO.LOW, GPIO.LOW, GPIO.HIGH]
+gpio_hl_list = [GPIO.LOW, GPIO.HIGH, GPIO.LOW, GPIO.HIGH, GPIO.LOW, GPIO.HIGH]
+gpio_hl_list2 = [GPIO.HIGH, GPIO.LOW, GPIO.HIGH, GPIO.LOW, GPIO.HIGH, GPIO.LOW]
 
 for c in channels:
-  GPIO.setup(c, GPIO.OUT)
+  GPIO.setup(c, GPIO.OUT, initial=GPIO.HIGH)
 
 mode = GPIO.getmode()
 
