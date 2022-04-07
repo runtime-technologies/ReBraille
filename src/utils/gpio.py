bd3ada3c99dc27aac6dc2262878e6a5b3e4ebc4d
Srijith <srijith96@gmail.com>
2022-04-07 00:12:01 -0400

    Add functionailty to await button input to the GPIO + some minor comments in the code

--- src/utils/gpio.py
+++ src/utils/gpio.py
@@ -20,23 +20,31 @@ High    -> Reverse
 
 BRAILLE_MATRIX_SIZE = 8
 
+BUTTON_POLL_INTERVAL = 0.2
 DIRECTION_PIN = 38
+BUTTON_INPUT_PIN = 22
 CONTROL_PINS = [11, 15, 19, 23, 29, 31, 35, 40]
 
 PINS_HIGH = [GPIO.HIGH] * BRAILLE_MATRIX_SIZE
 PINS_LOW = [GPIO.LOW] * BRAILLE_MATRIX_SIZE
 
 
+# initialization and set/reset/sleep state functions
 def initialize_gpio():
     """
     Initialises the GPIO pins on the Jetson.
     """
     GPIO.setmode(GPIO.BOARD)
 
+    # set direction pin as output, with init LOW
     GPIO.setup(DIRECTION_PIN, GPIO.OUT, initial=GPIO.LOW)
 
+    # set control pins as output, with init LOW
     for c in CONTROL_PINS:
         GPIO.setup(c, GPIO.OUT, initial=GPIO.LOW)
+
+    # set user button pin as input
+    GPIO.setup(BUTTON_INPUT_PIN, GPIO.IN)
 
     mode = GPIO.getmode()
 
@@ -67,27 +75,18 @@ def set_pins(pin_configuration=PINS_LOW):
     GPIO.output(DIRECTION_PIN, GPIO.LOW)
     GPIO.output(CONTROL_PINS, pin_configuration)
 
-
-def test_all_pins(interval=1, iterations=10):
+def await_button_input(verbose=False):
     """
-    Sets and resets all pins for testing for the specified number
-    of iterations with the specified interval.
+    Await for a button input.
 
-    Args:
-        interval (int, optional): Delay between each set and reset. Defaults to 1.
-        iterations (int, optional): Number of times to set and reset the pins. Defaults to 10.
+    Params:
+        verbose - display output
     """
-    for i in range(iterations):
-        set_pins(PINS_HIGH)
-        sleep(interval)
-        sleep_state()
-        sleep(interval)
-        reset_pins()
-        sleep(interval)
-        sleep_state()
-        sleep(interval)
+    while GPIO.input(BUTTON_INPUT_PIN) != GPIO.HIGH:
+        if verbose: print("Awaiting button press.")
+        sleep(BUTTON_POLL_INTERVAL)
 
-
+# functions to test and diplay Braille sequences
 def display_character(braille_code='00000000'):
     """
     Converts braille code to GPIO configuration.
@@ -98,3 +97,50 @@ def display_character(braille_code='00000000'):
     pin_configuration = [
         GPIO.HIGH if character is '1' else GPIO.LOW for character in braille_code]
     set_pins(pin_configuration)
+
+
+def test_all_pins(interval=1, iterations=10, verbose=True):
+    """
+    Sets and resets all pins for testing for the specified number
+    of iterations with the specified interval.
+
+    Args:
+        interval (int, optional): Delay between each set and reset. Defaults to 1.
+        iterations (int, optional): Number of times to set and reset the pins. Defaults to 10.
+        verbose (bool, optional): If true prints state to console
+    """
+    for i in range(iterations):
+        await_button_input(verbose)
+        if verbose: print("Set")
+        set_pins(PINS_HIGH)
+        sleep(interval)
+        sleep_state()
+        sleep(interval)
+
+        if verbose: print("Reset")
+        reset_pins()
+        sleep(interval)
+        sleep_state()
+        sleep(interval)
+
+
+def test_patterns(pattern_list, interval=1, iterations=10, verbose=True):
+    """
+    Loops over pattern list diplaying each for specified interval.
+
+    Args:
+        pattern_list (list): List of 8bit sequences for diplaying on the braille display.
+        interval (int, optional): Delay between each set and reset. Defaults to 1.
+        iterations (int, optional): Number of times to set and reset the pins. Defaults to 10.
+        verbose (bool, optional): If true, prints state to console
+    """
+    for i in range(iterations):
+        for c in pattern_list:
+            await_button_input(verbose)
+            if verbose: print(f"Displaying: {c}")
+
+            set_pins(PINS_HIGH)
+            sleep(interval)
+            reset_pins()
+            sleep(interval)
+
